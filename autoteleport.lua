baaeb6fdd574111e6a7f48fa793431b3260c1029
Weevely <managerkasft@gmail.com>
2025-07-17 22:09:43 -0700

    Update autoteleport.lua

--- autoteleport.lua
+++ autoteleport.lua
@@ -1,7 +1,7 @@
 local TeleportService = game:GetService("TeleportService")
 
-local placeId = 109983668079237
+local placeId = 109983668079237 -- your Roblox game's placeId
 
-local jobId = "00ed277d-4b06-4d48-915e-f36797d0bcde" -- This will be updated manually or by your C# tool
+local jobId = "00000000-0000-0000-0000-000000000000" -- placeholder JobId to be replaced
 
 TeleportService:TeleportToPlaceInstance(placeId, jobId, game.Players.LocalPlayer)
